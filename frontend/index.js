de74b92d0e51afe21a4987229b63dfc456ad9dcf
Michael Montalvo <mmontalvo93@outlook.com>
2023-11-08 19:22:26 -0400

    Take4

--- frontend/index.js
+++ frontend/index.js
@@ -80,7 +80,6 @@ function moduleProject1() {
       const friendId = person.friends[idx];
       const friend = people.find(p => p.id === friendId);
       const fullName = `${friend.fname} ${friend.lname}`
-      console.log(fullName);
       let isLastIdx = idx === person.friends.length - 1;
       let isNextToLastIdx = idx === person.friends.length - 2;
       if (isLastIdx) {
